a2230cece0214de439a0d8643f767f3009dd23c8
Maria Alice <nantesmalice@gmail.com>
2025-06-23 18:34:26 -0400

    Adding dependency injection abstraction package to the application layer

--- 1.GettingStarted/Movies.api/Program.cs
+++ 1.GettingStarted/Movies.api/Program.cs
@@ -1,9 +1,11 @@
+using Movies.Application;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
